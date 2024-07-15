8877731855f34022ebe241dc1c14b8f0b549b8e1
Łukasz Wodniak <gerdzio53@gmail.com>
2024-07-15 21:11:37 +0200

    Initial commit

---
+++ src/main/kotlin/com/lukaszwodniak/folky/FolkyApplication.kt
@@ -0,0 +1,11 @@
+package com.lukaszwodniak.folky
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FolkyApplication
+
+fun main(args: Array<String>) {
+	runApplication<FolkyApplication>(*args)
+}
---
+++ src/test/kotlin/com/lukaszwodniak/folky/FolkyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lukaszwodniak.folky
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FolkyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
