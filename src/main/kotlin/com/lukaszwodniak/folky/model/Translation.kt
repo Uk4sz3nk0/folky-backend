ec179efdf7e46cc224b51d608b4b33898f5100c2
Uk4sz3nk0 <58225253+Uk4sz3nk0@users.noreply.github.com>
2024-10-28 10:56:24 +0100

    [FEATURE] - Implementing Firebase cloud messaging (FCM) (#9)

---
+++ src/main/kotlin/com/lukaszwodniak/folky/model/Translation.kt
@@ -0,0 +1,21 @@
+package com.lukaszwodniak.folky.model
+
+import jakarta.persistence.*
+
+/**
+ * Translation
+ *
+ * Created on: 2024-10-28
+ * @author Łukasz Wodniak
+ */
+
+@Entity
+@Table(name = "translations_dictionary")
+data class Translation(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    val language: String,
+    val specifier: String,
+    val value: String
+)
