a648cea7d309c4277a6d2590f4e844571fdd9a70
Łukasz Wodniak <gerdzio53@gmail.com>
2024-10-28 17:13:21 +0100

    Setting proper version and clearing TODO list

--- src/main/kotlin/com/lukaszwodniak/folky/FolkyApplication.kt
+++ src/main/kotlin/com/lukaszwodniak/folky/FolkyApplication.kt
@@ -1,8 +1,6 @@
 package com.lukaszwodniak.folky
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
